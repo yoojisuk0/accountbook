4ed032d61f813e5b47de8e9c5a830b50d58860d5
yoojisuk <61814964+yoojisuk0@users.noreply.github.com>
2024-02-21 17:23:45 +0900

    commit

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo apt-get update
-sudo apt-get install net-tools
+sudo apt-get updatesudo apt-get install net-tools
 sudo apt install iputils-ping
 pip install httpie
 
@@ -18,5 +17,3 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install 14.19.0 && nvm use 14.19.0
 export NODE_OPTIONS=--openssl-legacy-provider
 
-cd infra
-docker-compose up
